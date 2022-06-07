65c6e72e00eb58ce552d809941f933a990529a15
Danilo <danilomartins.pacs@gmail.com>
2022-06-07 12:22:13 -0300

    feat: implement add hotel for address

--- src/modules/address/repositories/IAddressRepository.ts
+++ src/modules/address/repositories/IAddressRepository.ts
@@ -3,7 +3,13 @@ export type AddressRequest = {
   regionCode: string;
   cityName: string;
 };
+export type IHotelRequest = {
+  hotel_ids: string[];
+  address_id: string;
+};
 export default interface IAddressRepository {
   all(): Promise<Address[]>;
   create(address: AddressRequest[]): Promise<boolean>;
+  addHotel(address: IHotelRequest): Promise<boolean>;
+  findById(id: string): Promise<Address | null>;
 }
--- src/modules/address/repositories/implementations/MongooseAddressRepository.ts
+++ src/modules/address/repositories/implementations/MongooseAddressRepository.ts
@@ -1,9 +1,9 @@
 import AddressMongoose from '@modules/address/infra/mongoose/schemas/Address';
-import IAddressRepository from '../IAddressRepository';
+import IAddressRepository, { IHotelRequest } from '../IAddressRepository';
 
 export default class MongooseAddressRepository implements IAddressRepository {
   async all(): Promise<Address[]> {
-    const response = await AddressMongoose.find();
+    const response = await AddressMongoose.find().populate({ path: 'hotels' });
     return response;
   }
   async create(address: Address[]): Promise<boolean> {
@@ -12,5 +12,28 @@ export default class MongooseAddressRepository implements IAddressRepository {
       return true;
     }
     return response;
+  }
+  async findById(id: string): Promise<Address | null> {
+    const response = await AddressMongoose.findOne({ _id: id });
+    if (response) return response;
+    return null;
+  }
+  async addHotel(hotel: IHotelRequest): Promise<boolean> {
+    const response = await AddressMongoose.updateOne(
+      { _id: hotel.address_id },
+      {
+        $push: {
+          hotels: {
+            $each: hotel.hotel_ids,
+          },
+        },
+      },
+    );
+    console.log('###', response);
+
+    if (response) {
+      return true;
+    }
+    return false;
   }
 }
