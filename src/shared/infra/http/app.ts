4ebf8d8d19e482a94305719f18fd969c16cd96a5
Danilo <danilomartins.pacs@gmail.com>
2022-06-06 23:32:25 -0300

    chore: configure

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,11 +1,20 @@
 import '@shared/container';
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
+import { container } from 'tsyringe';
+import ILogger from '../logger/interfaces/ILogger';
 import routes from './routes';
-
 const app = express();
 
-app.use(routes);
+const logger = container.resolve<ILogger>('logger');
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+if (logger.http) app.use(logger.http());
+app.use(routes);
 app.get('/status', (r, res) => res.status(200).json({ message: 'OK' }));
 
 export default app;
